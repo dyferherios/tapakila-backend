a3a1baccbc86058b378fe26aa1aa2312838d7266
dyferherios <freddy.andriamanohiniaina@gmail.com>
2025-02-27 23:42:18 +0300

    feat: add getMethod for role, user, and country

--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -1,18 +1,19 @@
 import { Router } from 'express';
 import {
-  getUsers,
-  getUserById,
-  createUser,
-  updateUser,
-  deleteUser
+  UserController
 } from '../controllers/UserControllers.js'
 
 const router = Router();
 
-router.get("/", getUsers);
-router.get("/:id", getUserById);
-router.post("/", createUser);
-router.put("/:id", updateUser);
-router.delete("/:id", deleteUser);
+router.get("/", UserController.getUsers);
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await UserController.getUserById(req.params.id);
+    res.json(user);  // Retourner l'objet utilisateur
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "An error occurred while fetching the user" });
+  }
+});
 
 export default router;
