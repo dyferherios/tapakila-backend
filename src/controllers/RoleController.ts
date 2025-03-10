3cdd66b4ebe950eb093eb115eccef0b06b6489ff
dyferherios <freddy.andriamanohiniaina@gmail.com>
2025-03-10 21:11:41 +0300

    refactor (role): modify saveRole method, add new method deleteRoleById

--- src/controllers/RoleController.ts
+++ src/controllers/RoleController.ts
@@ -1,90 +1,98 @@
-import pool from '../db/datasource.js';
-import { Role } from '../entity/Role.js';
+import pool from "../db/datasource.js";
+import { Role } from "../entity/Role.js";
 
 class RoleController {
-
-    static getRoles = async (request: any, response: any) => {
-        try {
-            const results = await pool.query('SELECT * FROM role');
-            const roles: Role[] = results.rows.map((row) => {
-                return new Role(
-                    row.id.toString(),
-                    row.title,
-                    row.created_at,
-                    row.updated_at
-                );
-            });
-            response.status(200).json(roles);
-        } catch (error) {
-            console.error(error);
-            response.status(500).json({ error });
-        }
-    };
-    static getRoleById = async (roleId: string) => {
+  static getRoles = async (request: any, response: any) => {
     try {
-        const result = await pool.query('SELECT * FROM role WHERE id = $1', [roleId]);
-        if (result.rows.length === 0) {
-            throw new Error('Role not found');
-        }
-            const role = result.rows[0];
-            const roleObject = new Role(
-                role.id.toString(),
-                role.title,
-                role.created_at,
-                role.updated_at
-            );
-            return roleObject;
-        } catch (error) {
-            console.error(error);
-            throw error;
-        }
-    };
+      const results = await pool.query("SELECT * FROM role");
+      const roles: Role[] = results.rows.map((row) => {
+        return new Role(
+          row.id.toString(),
+          row.title,
+          row.created_at,
+          row.updated_at
+        );
+      });
+      response.status(200).json(roles);
+    } catch (error) {
+      console.error(error);
+      response.status(500).json({ error });
+    }
+  };
+  static getRoleById = async (roleId: string) => {
+    try {
+      const result = await pool.query("SELECT * FROM role WHERE id = $1", [
+        roleId,
+      ]);
+      if (result.rows.length === 0) {
+        throw new Error("Role not found");
+      }
+      const role = result.rows[0];
+      const roleObject = new Role(
+        role.id.toString(),
+        role.title,
+        role.created_at,
+        role.updated_at
+      );
+      return roleObject;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  };
 
-    static saveRole = async (request: any, response: any) => {
+  static saveRole = async (request: any, response: any) => {
     const { id, title } = request.body;
 
     try {
-        const result = await pool.query('SELECT * FROM role WHERE id = $1', [id]);
+      const result = await pool.query("SELECT * FROM role WHERE id = $1", [id]);
 
-        if (result.rows.length > 0) {
-            const role = result.rows[0];
-            await pool.query(
-                'UPDATE role SET title = $1, updated_at = NOW() WHERE id = $2',
-                [title, id]
-            );
+      if (result.rows.length > 0) {
+        const role = result.rows[0];
+        await pool.query(
+          "UPDATE role SET title = $1, updated_at = NOW() WHERE id = $2",
+          [title, id]
+        );
 
-            const updatedRole = new Role(
-                role.id.toString(),
-                title,
-                role.created_at,
-                new Date() 
-            );
+        const updatedRole = new Role(
+          role.id.toString(),
+          title,
+          role.created_at,
+          new Date()
+        );
 
-            return response.status(200).json(updatedRole);
-        } else {
-            const newRole = await pool.query(
-                'INSERT INTO role (id, title, created_at, updated_at) VALUES ($1, $2, NOW(), NOW()) RETURNING *',
-                [id, title]
-            );
+        return response.status(200).json(updatedRole);
+      } else {
+        const newRole = await pool.query(
+          "INSERT INTO role (title, created_at, updated_at) VALUES ($1, NOW(), NOW()) RETURNING *",
+          [title]
+        );
 
-            const createdRole = newRole.rows[0];
-            const roleObject = new Role(
-                createdRole.id.toString(),
-                createdRole.title,
-                createdRole.created_at,
-                createdRole.updated_at
-            );
+        const createdRole = newRole.rows[0];
+        const roleObject = new Role(
+          createdRole.id.toString(),
+          createdRole.title,
+          createdRole.created_at,
+          createdRole.updated_at
+        );
 
-            return response.status(201).json(roleObject);
-        }
+        return response.status(201).json(roleObject);
+      }
     } catch (error) {
-        console.error(error);
-        response.status(500).json({ error: 'An error occurred while saving/updating the role' });
+      console.error(error);
+      response
+        .status(500)
+        .json({ error: "An error occurred while saving/updating the role" });
     }
-};
+  };
 
+    static deleteRoleById = async (roleId: string) => {
+        try {
+            await pool.query('DELETE FROM role WHERE id=$1', [roleId]);
+        } catch (error) {
+            throw error;
+        }
+    }
 }
 
-export {
-    RoleController,
-};
+export { RoleController };
--- src/routes/RoleRoutes.ts
+++ src/routes/RoleRoutes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import {
   RoleController
 } from '../controllers/RoleController.js'
+import { Role } from '../entity/Role.js';
 
 const router = Router();
 
@@ -15,5 +16,13 @@ router.get("/:id", async (req, res) => {
   }
 })
 router.post("/", RoleController.saveRole); 
+router.delete("/:id", async (req, res) => {
+  try {
+    await RoleController.deleteRoleById(req.params.id);
+    res.status(201).json("Role deleted succefully");
+  } catch (error) {
+    res.status(500).json({ error: "An error occured while deleting role" });
+  }
+})
 
 export default router;
